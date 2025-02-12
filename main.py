83e889f8a1783f1d0643a8d41fc7e54f493c3fe8
Julien MITERNIQUE <julien.miternique@piktura.fr>
2025-02-12 15:53:08 +0100

    ui dev

---
+++ main.py
@@ -0,0 +1,18 @@
+import sys
+from PySide6 import QtWidgets
+from ui.mainWidget import MainWidget
+
+def __main__():
+    app = QtWidgets.QApplication(sys.argv)
+
+    app.setStyle("fusion")
+
+    # V load and apply the application style
+
+    wid = MainWidget()
+
+    wid.show()
+
+    app.exec()
+
+__main__()
